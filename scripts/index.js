ba76fba30707c9e2007430cdc172b06c696c9e4d
N.SH <sky.blush@ya.ru>
2021-10-10 14:45:08 +0300

    Fix all bugs after first reView

--- scripts/index.js
+++ scripts/index.js
@@ -5,52 +5,33 @@ const editButton = document.querySelector('.profile__info-edit');
 const formButton = document.querySelector('.popup__button');
 const form = document.querySelector('.popup__form');
 
-const nameField = document.querySelector('.popup__input_name');
+const nameField = document.querySelector('.popup__input_type_name');
 const profileName = document.querySelector('.profile__info-name');
 
-const descriptionField = document.querySelector('.popup__input_description');
+const descriptionField = document.querySelector('.popup__input_type_description');
 const profileDescription = document.querySelector('.profile__info-description');
 
-nameField.value = profileName.textContent;
-descriptionField.value = profileDescription.textContent;
-
-
-const like = document.querySelectorAll('.place__like');
-
-function likeActive(i) {
-  if (like[i].classList.contains('place__like_active')) {
-      like[i].classList.remove('place__like_active')
-  } else {
-      like[i].classList.add('place__like_active')
-  }
-}
-
-for (let i = 0; i < like.length; i++) {
-  like[i].addEventListener('click', function() {
-    likeActive(i)
-  })
-}
-
-
 function openPopup() {
-    popup.classList.add('popup_isOpen')
+    nameField.value = profileName.textContent;
+    descriptionField.value = profileDescription.textContent;
+    popup.classList.add('popup_open');
 }
 
 function closePopup() {
-    popup.classList.remove('popup_isOpen')
+    popup.classList.remove('popup_open')
 }
 
 editButton.addEventListener('click', openPopup)
 
 popupCloseButton.addEventListener('click', closePopup)
 
-function popupClickHandler(event) {
-    if (event.target.classList.contains('popup')) {
-        closePopup()
-    }
-}
+// function popupClickHandler(event) {
+//     if (event.target.classList.contains('popup')) {
+//         closePopup()
+//     }
+// }
 
-popup.addEventListener('mouseup', popupClickHandler)
+// popup.addEventListener('mouseup', popupClickHandler)
 
 function submitForm(event) {
     event.preventDefault()
@@ -62,3 +43,19 @@ function submitForm(event) {
 }
 
 form.addEventListener('submit', submitForm)
+
+// const like = document.querySelectorAll('.place__like');
+
+// function likeActive(i) {
+//   if (like[i].classList.contains('place__like_active')) {
+//       like[i].classList.remove('place__like_active')
+//   } else {
+//       like[i].classList.add('place__like_active')
+//   }
+// }
+
+// for (let i = 0; i < like.length; i++) {
+//   like[i].addEventListener('click', function() {
+//     likeActive(i)
+//   })
+// }
