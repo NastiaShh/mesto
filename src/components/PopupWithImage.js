65ea56ce06ae24695ab1100ddd8fd1eefb3ff12a
N.SH <sky.blush@ya.ru>
2022-01-07 14:44:33 +0300

    refactor class PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,13 +4,14 @@ export class PopupWithImage extends Popup {
   constructor(popupConfig, popupSelector){
     super(popupConfig, popupSelector)
     this._figure = this._popup.querySelector(this._popupConfig.figureSelector)
+    this._figcaption = this._figure.querySelector(this._popupConfig.placeCaptionSelector)
     this._image = this._figure.querySelector(this._popupConfig.placePopupImageSelector)
   }
 
   open(src, name) {
     this._image.src = src
     this._image.alt = name
-    this._figure.querySelector(this._popupConfig.placeCaptionSelector).textContent = name
+    this._figcaption.textContent = name
 
     super.open();
   }
